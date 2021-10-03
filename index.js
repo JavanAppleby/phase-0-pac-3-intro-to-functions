09686b66abcd79abc0de0969ccbf006a36f5887f
JavanAppleby <styx_n_stonz@hotmail.com>
2021-10-04 07:16:19 +1000

    adding function examples

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Follow along with the examples here
+function doNothing() {}
+
+function sayHello() {
+    console.log('Hello!');
+  }
+
+sayHello();
+
+function sayHelloToIsabel() {
+console.log("Hello, Isabel!");
+}
+
+function sayHelloToSofia() {
+    console.log("Hello, Sofia!");
+}
+
+function sayHelloToBrendan() {
+    console.log("Hello, Brendan!");
+}
+    
+sayHelloToIsabel();
+sayHelloToSofia();
+sayHelloToBrendan();
